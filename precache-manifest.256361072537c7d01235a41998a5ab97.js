cd380b6acb662999099766885c50d9157b6809ba
liuzhengwei <liuzhengwei@z-group.cn>
2021-03-30 00:01:20 +0800

    .

---
+++ precache-manifest.256361072537c7d01235a41998a5ab97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8a29ba300f74deeeb4",
+    "url": "/static/css/main.5d1ce6eb.chunk.css"
+  },
+  {
+    "revision": "ca8a29ba300f74deeeb4",
+    "url": "/static/js/main.1a5e8cdf.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "33adfe2959d1158770a2",
+    "url": "/static/css/2.fe835ff0.chunk.css"
+  },
+  {
+    "revision": "33adfe2959d1158770a2",
+    "url": "/static/js/2.97433415.chunk.js"
+  },
+  {
+    "revision": "feefcf3b8520511d1a92e3e2b037591a",
+    "url": "/index.html"
+  }
+];
